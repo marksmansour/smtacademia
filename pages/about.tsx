520be597466b2298f0e36169f4c4cbfb174bf2d9
Mark Mansour <markmansour01@gmail.com>
2023-12-11 14:25:39 +0000

    add pages

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Layout from 'components/Layout'
+import PageHeader from 'components/PageHeader'
+
+export default function Page() {
+  return (
+    <Layout>
+      <PageHeader title="About" />
+    </Layout>
+  )
+}
